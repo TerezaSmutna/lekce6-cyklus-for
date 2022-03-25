e3c32575786e5b978c2c759102896e5e3a9091ab
LAPTOP-5LG8T5MU\terka <terka.kop@seznam.cz>
2022-03-25 20:34:29 +0100

    Lesson 6 - While loop

--- main.js
+++ main.js
@@ -1,31 +1,25 @@
-// Použij výpis do konzole prohlížeče pomocí console.log('text')
-// Konzoli otevřeš v Chrome stiskem F12 (na Windows) nebo Command-Option-I (na Macu)
+console.log('-------------------');
 
+let a = 0
+while (a <= 10) {
+  console.log(a);
+  a++;
+}
 
-// První příklad - vypiš vzestupně čísla od 0 do 10
-console.log('První příklad - vzestupná řada 0 - 10');
-
-//TADY NAPIS CYKLUS
 
 console.log('-------------------');
 
+let c = 0;
+while (c < 11) {
+  console.log(c);
+  c++;
+}
 
-
-// Druhý příklad - zkopíruj předchozí příklad
-// a vyzkoušej, jaký je rozdíl mezi <= a <
-// v podmínce uvnitř cyklu
-
-console.log('Druhý příklad - rozdíl mezi <= a <');
-
-//TADY NAPIS CYKLUS
 
 console.log('-------------------');
 
-
-
-// Třetí příklad - vypiš sestupně čísla od 10 do 0
-console.log('Třetí příklad - sestupná řada 10 - 0');
-
-//TADY NAPIS CYKLUS
-
-console.log('-------------------');
+let b = 10
+while (b >= 0) {
+  console.log(b);
+  b = b - 1;
+}
