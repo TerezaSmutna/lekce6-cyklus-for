15ad95a3e6c4a15f1144917da041e21b3a7cf501
LAPTOP-5LG8T5MU\terka <terka.kop@seznam.cz>
2022-03-25 21:02:17 +0100

    Lesson 6 - For loop

--- main.js
+++ main.js
@@ -1,31 +1,19 @@
-// Použij výpis do konzole prohlížeče pomocí console.log('text')
-// Konzoli otevřeš v Chrome stiskem F12 (na Windows) nebo Command-Option-I (na Macu)
-
-
-// První příklad - vypiš vzestupně čísla od 0 do 10
-console.log('První příklad - vzestupná řada 0 - 10');
-
-//TADY NAPIS CYKLUS
-
 console.log('-------------------');
 
 
-
-// Druhý příklad - zkopíruj předchozí příklad
-// a vyzkoušej, jaký je rozdíl mezi <= a <
-// v podmínce uvnitř cyklu
-
-console.log('Druhý příklad - rozdíl mezi <= a <');
-
-//TADY NAPIS CYKLUS
+for (let i = 0; i <= 10; i = i + 1) {
+  console.log(i);
+}
 
 console.log('-------------------');
 
+for (let i = 0; i < 11; i = i + 1) {
+  console.log(i);
+}
 
-
-// Třetí příklad - vypiš sestupně čísla od 10 do 0
-console.log('Třetí příklad - sestupná řada 10 - 0');
-
-//TADY NAPIS CYKLUS
 
 console.log('-------------------');
+
+for (let i = 10; i >= 0; i = i - 1) {
+  console.log(i);
+}
